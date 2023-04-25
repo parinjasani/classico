1bf6e8006b04443d5555829f8b4fb68d16e4d43c
ASUS <parinjasani0@gmail.com>
2023-04-25 13:11:44 +0530

    container and its propertties

--- lib/container-dart.dart
+++ lib/container-dart.dart
@@ -11,8 +11,59 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Container'),
         ),
-        body: Container(
-            child: Text('Hello World'),
+        body:  Center(
+          child: Container(
+                alignment: Alignment.center,
+               // padding: EdgeInsets.only(left: 10,top: 20),
+                margin: EdgeInsets.only(left: 10,top: 10),
+                width: 200,
+                height: 200,
+               // color: Colors.green,
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  //borderRadius: BorderRadius.circular(20),
+                  // borderRadius: BorderRadius.vertical(
+                  //   top: Radius.circular(20),
+                  // )
+                  // borderRadius: BorderRadius.only(
+                  //   topLeft: Radius.circular(20)
+                  // )
+                  boxShadow:
+                    [
+                      BoxShadow(
+                        color: Colors.black38,
+                        blurRadius: 20,
+                        spreadRadius: 10,
+                        offset: Offset(2,3),
+
+                      )
+                    ],
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.red.shade300,
+                      Colors.orange.shade300,
+                      Colors.yellow.shade300,
+                    ],
+                       begin: Alignment.topLeft,
+                       end: Alignment.bottomLeft,
+                     // tileMode: TileMode.decal,
+                  ),
+                  border: Border.all(
+                    color: Colors.yellow.shade50,
+                    width: 1,
+                    strokeAlign: BorderSide.strokeAlignInside
+                  ),
+                ),
+                //transform: Matrix4.rotationY(4),
+                //constraints: BoxConstraints.expand(width: 200),
+                child: Text('Parin jasani',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+            ),
         ),
       ),
     );
