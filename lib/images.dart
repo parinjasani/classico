cb70ba404642cacee5b387a6feb1e79f7d6ed42f
ASUS <parinjasani0@gmail.com>
2023-04-26 21:22:26 +0530

    how to insert images in dart

---
+++ lib/images.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image'),
+        ),
+        body: Center(
+         child:Image.asset(
+             'assets/images/IMG_7947.jpeg',
+                 width: 300,
+                 height: 600,
+         ),
+          ),
+        ),
+    );
+  }
+}
