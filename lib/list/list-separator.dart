557712e25f0ad422c674613dad75389fbe6fda1a
ASUS <parinjasani0@gmail.com>
2023-04-29 10:52:44 +0530

    list view in flutter

--- lib/list/list-separator.dart
+++ lib/list/list-separator.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home:Myhomepage(),
     );
   }
